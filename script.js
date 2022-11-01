032611deef31b120eb6f9ab2aae4d1b50ba268b0
vandit-bera <vanditbera@gmail.com>
2022-11-01 18:12:01 +0530

    Add comment for information

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
+/* Target a Element */
 let text = document.querySelector("#input");
 
 let btn = document.querySelector(".check");
 
 let output = document.querySelector(".output");
 
+/* checking function */
 function checking(){
+    /* Fetch input value */
     let inp = text.value;
+    /* Convert string to array */
     let input = inp.split('');
+    /* Reverse array */
     let revInp = input.reverse();
+    /* Convert array to string */
     let revInput = revInp.join('');
 
+    /* Check if given value and reverse value are same or not */
     if(inp == revInput){
         output.innerText = "It is a Palindrome"
     } else {
         output.innerText = "It is NOT a Palindrome"
     }
 
+    /* Check if user doesn't give the any value into the input */
     if(inp.length == 0){
         output.innerText = "Please Enter Input!"
     }
 }
 
+/* Add click event listener */
 btn.addEventListener("click", checking)
